90eb694f288e6ccafc776fba839a6387789750d8
romulo garofalo <romulogarofalo@gmail.com>
2020-09-22 06:23:16 +0000

    added print message of the attack

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -11,4 +11,13 @@ defmodule ExMon.Game.Status do
     IO.puts("\n==== Invalid move: #{move} ====")
   end
 
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("==== the player attacked the computer dealing #{damage} damage ====")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("==== the player attacked the computer dealing #{damage} damage ====")
+  end
+
+
 end
