96bd51152486ec5696826486ef55f1bb9711ec68
romulo garofalo <romulogarofalo@gmail.com>
2020-09-22 05:55:00 +0000

    added moves logic

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,13 +1,36 @@
 defmodule ExMon do
   alias ExMon.{Player, Game}
+  alias ExMon.Game.Status
+  alias ExMon.Actions
   def create_player(name, move_avg, move_heal, move_rnd) do
     Player.build(name, move_avg, move_heal, move_rnd)
   end
 
+  @computer_name "robo"
 
   def start_game(player) do
-    "robo"
+    @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+
+    Status.print_round_message()
   end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "make heal"
+      move -> Actions.attack(move)
+    end
+  end
+
+
 end
